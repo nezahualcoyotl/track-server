08b17075d0686350413478218b3a1d3816f1d51e
Damian Medina <damian@nemesys.io>
2022-04-26 22:50:23 -0500

    IMPORTANT CORRECTION

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const userSchema = new moongose.Schema({
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         unique: true,
