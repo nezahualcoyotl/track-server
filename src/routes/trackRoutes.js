c9b7af4ca82ee8f820ccb7599d9a89d5db4b25d5
Damian Medina <damian@nemesys.io>
2022-04-27 22:53:25 -0500

    post method to save tracks

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -14,4 +14,20 @@ router.get('/tracks', async (req, res) => {
     res.send(tracks)
 })
 
+router.post('/tracks', async (req, res) => {
+    const { name, locations } = req.body
+
+    if (!name || !locations) {
+        return res.status(422).send({ error: 'You must provide a name and locations'})
+    }
+
+    try {
+        const track = new Track({ name, locations, userId: req.user._id })
+        await track.save()
+        res.send(track)
+    } catch (error) {
+        res.status(422).send({ error: error.message })
+    }
+})
+
 module.exports = router
